fb9a5c92f55f0c20b8c7fad5bdaa321a3c863544
liahu <testyida@163.com>
2019-08-22 13:22:13 +0800

    备份

---
+++ themes/anatole/source/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?5049c5b96a4f3cb2618730846a60422a";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
